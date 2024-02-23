5c6f45797f800f9f8292883fb8f53c683feeb4e2
fengqixian <fengqixian@threegene.com>
2024-02-23 10:30:01 +0800

    1

--- miniprogram/api/wechat.js
+++ miniprogram/api/wechat.js
@@ -1,9 +1,15 @@
 import {
     request
-  } from '../utils/request';
- 
-  export const getWechatMessageTemplateIds = () => {
+} from '../utils/request';
+
+export const getWechatMessageTemplateIds = () => {
     return request({
-      url: '/wechat/message/template/ids'
+        url: '/wechat/message/template/ids'
     })
-  }
+}
+
+export const wechatMiniLoginPC = (loginCode) => {
+    return request({
+        url: '/wechat/login/' + loginCode
+    })
+}
